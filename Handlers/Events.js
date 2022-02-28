e048ea1a1d71710c8ba8c45b36e6fe913b5b0f01
Aestriex <68126194+aestriex@users.noreply.github.com>
2022-02-28 14:13:11 -0800

    Basis for event handler

---
+++ Handlers/Events.js
@@ -0,0 +1,5 @@
+const { Events } = require("../Validation/EventNames");
+
+module.exports = async (client, PG) => {
+    
+}
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { Client } = require("discord.js");
 const client = new Client({intents: 32767});
 const { Token, DatabaseURL } = require("./config.json");
 
+const { promisify } = require("util");
+const { glob } = require("glob");
+const PG = promisify(glob);
+
 client.once("ready", () => {
     console.log(`Client is online!`);
 });
