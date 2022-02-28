c1a34aa060356bb4d3ac559aa293fc1fa2162afe
Aestriex <68126194+aestriex@users.noreply.github.com>
2022-02-28 13:41:11 -0800

    Config for v13

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const {  } = require("discord.js")
+const { Client } = require("discord.js");
+const client = new Client({intents: 32767});
+const { Token, DatabaseURL } = require("./config.json");
+
+client.once("ready", () => {
+    console.log(`Client is online!`);
+});
+
+client.login(Token);
